7924da87be58b5756740b1ec3101fc795d7693ae
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-11-21 16:02:53 +0200

    fix

--- tests/production/config/database.js
+++ tests/production/config/database.js
@@ -7,7 +7,6 @@ module.exports = ({ env }) => ({
       database: env("DATABASE_NAME"),
       user: env("DATABASE_USERNAME"),
       password: env("DATABASE_PASSWORD"),
-      schema: env("DATABASE_SCHEMA", "public"), // Not required
       // ssl: {
       //   rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
       // },
