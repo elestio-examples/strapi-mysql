006204949c24db6369fa7b26d8adacf21a475eba
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-11-21 13:42:47 +0200

    fix

--- tests/production/build.sh
+++ tests/production/build.sh
@@ -4,5 +4,6 @@ cp ./.dockerignore ./app/.dockerignore
 cp ./Dockerfile ./app/Dockerfile
 cp ./.env ./app/.env
 cp ./entrypoint.sh ./app/entrypoint.sh
+chmod +x ./app/entrypoint.sh
 
 cd ./app && touch yarn.lock && yarn install && docker buildx build . --output type=docker,name=elestio4test/strapi-mysql-production:latest | docker load && cd ..
