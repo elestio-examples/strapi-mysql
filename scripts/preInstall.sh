a38d500bf424bebcc1b462082bc85a7e5e3a5e8c
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-11-23 13:16:03 +0200

    fix

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -11,20 +11,3 @@ yarn add @strapi/provider-email-nodemailer
 EOT
 
 chmod +x ./entrypoint.sh
-
-cat <<EOT > ./servers.json
-{
-    "Servers": {
-        "1": {
-            "Name": "local",
-            "Group": "Servers",
-            "Host": "db",
-            "Port": 5432,
-            "MaintenanceDB": "postgres",
-            "SSLMode": "prefer",
-            "Username": "postgres",
-            "PassFile": "/pgpass"
-        }
-    }
-}
-EOT
