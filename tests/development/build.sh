ae21edc5005d4d30529f7d7d17108c0109926fb2
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-09-24 17:40:58 +0300

    fix

--- tests/development/build.sh
+++ tests/development/build.sh
@@ -1,4 +1,4 @@
-yes | npx create-strapi-app@latest app --dbforce --no-run --dbclient=mysql --dbhost=db --dbport=3306 --dbname=strapi --dbusername=strapi --dbpassword=strapi
+npx create-strapi-app@latest app --skip-cloud --js --install --no-git-init --example --no-run --dbclient=mysql --dbhost=db --dbport=3306 --dbname=strapi --dbusername=strapi --dbpassword=strapi
 
 cp ./.dockerignore ./app/.dockerignore
 cp ./Dockerfile ./app/Dockerfile
